014a83804369d62d0d707646193a788937a77f34
kbull136 <kbull136@mtroyal.ca>
2024-03-27 11:57:31 -0600

    fixed issue with select not working

--- Asg2/src/components/HeaderApp.jsx
+++ Asg2/src/components/HeaderApp.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import HeaderSeasonSelect from "./HeaderSeasonSelect";
 import HeaderMenu from "./HeaderMenu";
+import { useAsyncError } from "react-router";
 
 const HeaderApp = (props) => {
 
     //fetch seasons here
     const [seasons, setSeasons] = useState([]);
+
 
     useEffect(() => {
         const url = "https://zest-jolly-advantage.glitch.me/api/seasons";
@@ -14,10 +16,10 @@ const HeaderApp = (props) => {
             .then(resp => resp.json())
             .then(data => setSeasons(data))
     }, []);
-
+    
     return (
         <header className="header">
-            <HeaderSeasonSelect seasons={seasons} />
+            <HeaderSeasonSelect seasons={seasons}/>
             <HeaderMenu />
         </header>
     );
--- Asg2/src/components/HeaderSeasonSelect.jsx
+++ Asg2/src/components/HeaderSeasonSelect.jsx
@@ -1,40 +1,41 @@
 import { useState } from "react";
-import SeasonOption from "./SeasonOption";
-import { FormControl, InputLabel, Select } from "@mui/material";
+import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import { Box } from "@mui/system";
 
 const HeaderSeasonSelect = (props) => {
 
-    const [season, setSeason] = useState("");
+    const [season, setSeason] = useState(''); 
 
     const handleChange = (e) => {
-        //insert handle code here
-        setSeason(e.target.value);
+
+        const selectedSeason = e.target.value;
+
+        setSeason(selectedSeason);
+
+        console.log(season);
 
     }
 
     return (
-        // <FormControl sx={{ m: 2, minWidth: 180 }}>
-        //     <InputLabel id="season-label">Choose Season</InputLabel>
-        //     <Select
-        //         labelId="season-label"
-        //         id="season-select"
-        //         value={season}
-        //         autoWidth
-        //         label="Choose Season"
-        //         onChange={handleChange}
-        //     >
-        //         {props.seasons.map(s => <SeasonOption key={s.year} season={s} />)}
-        //     </Select>
-        // </FormControl>
-
-        <div>
-            <h2>Season</h2>
-            <select onChange={handleChange}>
-                <option hidden={true}>Choose Season</option>
-                {props.seasons.map(s => <SeasonOption key={s.year} season={s} />)}
-            </select>
-        </div>
-
+        <Box>
+        <FormControl sx={{ m: 2, minWidth: 180 }}>
+            <InputLabel id="season-label">Choose Season</InputLabel>
+            <Select
+                labelId="season-label"
+                id="season-select"
+                value={season}
+                autoWidth
+                label="Choose Season"
+                onChange={handleChange}
+            >
+                {props.seasons.map((s, indx) => {
+                    <MenuItem key={indx} value={s.year}>
+                        {s.year}
+                    </MenuItem>
+                })}
+            </Select>
+        </FormControl>
+        </Box>
     )
 }
 

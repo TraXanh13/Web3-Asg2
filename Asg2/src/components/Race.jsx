a75c26ca7ae6e65404f512265cc6e3969e7803fe
Kyle Duong <kyleduong13@gmail.com>
2024-03-31 05:27:11 -0600

    Tracks have clickable links to the appropriate wiki

--- Asg2/src/components/Race.jsx
+++ Asg2/src/components/Race.jsx
@@ -1,16 +1,16 @@
 import {Link} from 'react-router-dom'
 
 const Race = (props) => {
-
+    console.log(props)
     return (
         <div className="flex justify-between">
-            <div className="min-w-52">{props.index}. {props.race.name}</div> 
+            <a href={props.race.url} target="_blank" rel="noopener noreferrer" className="min-w-52">{props.index}. {props.race.name}</a> 
             <div className="mx-12"> {props.race.date}</div>
             <div>
                 <Link to="/">
                     <button className="mr-2 border rounded px-2 hover:bg-red-700 hover:text-white">Results</button>
                 </Link>
-                
+
                 <Link to="/">
                     <button className="border rounded px-2 hover:bg-red-700 hover:text-white">Standings</button>
                 </Link>

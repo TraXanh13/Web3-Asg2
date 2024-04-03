ed3e4bf5b7a1c803790971f7c9794135be1e888c
kbull136 <kbull136@mtroyal.ca>
2024-04-03 12:27:46 -0600

    working on header

--- Asg2/src/components/HeaderSeasonSelect.jsx
+++ Asg2/src/components/HeaderSeasonSelect.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
-import { Box } from "@mui/system";
+import SeasonOption from "./SeasonOption.jsx";
 
 const HeaderSeasonSelect = (props) => {
 
-    const [season, setSeason] = useState(''); 
+    const [season, setSeason] = useState('');
 
     const handleChange = (e) => {
         setSeason(e.target.value);
@@ -12,23 +11,10 @@ const HeaderSeasonSelect = (props) => {
     }
 
     return (
-        <Box>
-        <FormControl sx={{ m: 2, minWidth: 180 }}>
-            <InputLabel id="season-label">Choose Season</InputLabel>
-            <Select
-                labelId="season-label"
-                id="season-select"
-                value={season}
-                autoWidth
-                label="Choose Season"
-                onChange={handleChange}
-            >
-                {props.seasons.map((s, indx) => 
-                    <MenuItem key={indx} value={s.year}>{s.year}</MenuItem>
-                )}
-            </Select>
-        </FormControl>
-        </Box>
+        <select onChange={handleChange}>
+            <option hidden={true}>Choose Season</option>
+            {props.seasons.map(s => <SeasonOption key={s.year} season={s} />)}
+        </select>
     )
 }
 
--- Asg2/src/components/login.jsx
+++ Asg2/src/components/login.jsx
@@ -1,75 +1,75 @@
 // /*
 //  Login form template: https://tailwindui.com/components/application-ui/forms/sign-in-forms
 // */
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
     return (
-        <div className = "absolute w-screen h-screen bg-cover bg-[url('/images/raceCarDriver.jpg')] overflow-hidden">
+        <div className="absolute w-screen h-screen bg-cover bg-[url('/images/raceCarDriver.jpg')] overflow-hidden">
             <div className="flex justify-center align-middle">
                 <div className=" min-h-auto px-6 py-6 w-fit my-24 rounded-md bg-white">
                     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
                         <img
-                        className="mx-auto h-24 w-auto"
-                        src="/images/F1_Fusion_Logo.svg"
-                        alt="f1 fusion"
+                            className="mx-auto h-24 w-auto"
+                            src="/images/F1_Fusion_Logo.svg"
+                            alt="f1 fusion"
                         />
                         <h2 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900">
-                        Sign in to your account
+                            Sign in to your account
                         </h2>
                     </div>
 
                     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
                         <form className="space-y-6" action="#" method="POST">
-                        <div>
-                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                            Email address
-                            </label>
-                            <div className="mt-2">
-                            <input
-                                id="email"
-                                name="email"
-                                type="email"
-                                autoComplete="email"
-                                // required
-                                className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
+                            <div>
+                                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Email address
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        id="email"
+                                        name="email"
+                                        type="email"
+                                        autoComplete="email"
+                                        // required
+                                        className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
                             </div>
-                        </div>
 
-                        <div>
-                            <div className="flex items-center justify-between">
-                            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                Password
-                            </label>
-                            <div className="text-sm">
-                                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                                Forgot password?
-                                </a>
+                            <div>
+                                <div className="flex items-center justify-between">
+                                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Password
+                                    </label>
+                                    <div className="text-sm">
+                                        <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                            Forgot password?
+                                        </a>
+                                    </div>
+                                </div>
+                                <div className="mt-2">
+                                    <input
+                                        id="password"
+                                        name="password"
+                                        type="password"
+                                        autoComplete="current-password"
+                                        // required
+                                        className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
                             </div>
-                            </div>
-                            <div className="mt-2">
-                            <input
-                                id="password"
-                                name="password"
-                                type="password"
-                                autoComplete="current-password"
-                                // required
-                                className="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-                        </div>
 
-                        <div>
-                            <Link to='/races/2023'>
-                                <button
-                                type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                                >
-                                Sign in
-                                </button>
-                            </Link>
-                        </div>
+                            <div>
+                                <Link to='/dashboard'>
+                                    <button
+                                        type="submit"
+                                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        Sign in
+                                    </button>
+                                </Link>
+                            </div>
                         </form>
                     </div>
                 </div>

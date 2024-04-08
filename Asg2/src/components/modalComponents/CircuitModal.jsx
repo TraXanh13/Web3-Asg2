f9b1c196b206a17d96e82f02af49f7ffbeb5eba1
Kyle Duong <kyleduong13@gmail.com>
2024-04-08 05:39:43 -0600

    Added circuit maps

--- Asg2/src/components/modalComponents/CircuitModal.jsx
+++ Asg2/src/components/modalComponents/CircuitModal.jsx
@@ -1,9 +1,10 @@
 import { useState, useContext, Fragment, useEffect } from "react";
 import { Dialog, Transition } from '@headlessui/react'
 import { AppContext } from "../../F1Context";
-import {MapContainer, TileLayer} from 'react-leaflet'
+import {TileLayer, Marker, Popup, MapContainer} from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
 import Button from "../functionalComponents/Button";
+import './circuitMap.css'
 
 const CircuitModal = (props) => {
     //This state is temporary and is use for testing, please change this to a proper one 
@@ -21,21 +22,23 @@ const CircuitModal = (props) => {
     useEffect(() => {
         if (circuit) {
             setIsDataLoaded(true);
-            console.log(circuit[0].lat)
         }
 
+        
         //check if the current circuit is in the favorites
         const isInFavorites = favoriteCircuits.some(favCircuit => favCircuit.circuitRef === circuit[0].circuitRef);
-
+        
         setInFavorites(isInFavorites);
+        
+        setTimeout(function () {
+            window.dispatchEvent(new Event("resize"));
+        }, 500);
 
     }, [circuit, favoriteCircuits]);
 
     const handleCircuitClose = () => {
         setCircuitView(false);
     }
-<<<<<<< HEAD
-=======
 
     const addToFavorites = () => {
 
@@ -45,7 +48,6 @@ const CircuitModal = (props) => {
         }
     }
 
->>>>>>> main
     return (
         <>
             {/*Please change the show to the right state name after testing */}
@@ -97,13 +99,19 @@ const CircuitModal = (props) => {
                                     {isDataLoaded ? (
                                         <>
                                             <div className="flex w-fit p-4 text-white">
-                                                <h1 className="flex-row text-center px-2">Circuit Image</h1>
+                                                {/* <h1 className="flex-row text-center px-2">Circuit Image</h1> */}
+                                                <img className="circuitImg mr-4" src="https://placeholder.co/200"></img>
                                                 {/* <h1 className="flex-row text-center" >Circuit Map</h1> */}
                                                 <MapContainer center={[circuit[0].lat, circuit[0].lng]} zoom={13}>
                                                     <TileLayer 
                                                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                                                     />
+                                                    <Marker position={[circuit[0].lat, circuit[0].lng]}>
+                                                        <Popup>
+                                                            <h2>{circuit[0].name}</h2>
+                                                        </Popup>
+                                                    </Marker>
                                                 </MapContainer>
                                             </div>
                                             <div className="w-fit p-4 text-white">
